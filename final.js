5a2628cb9367535e8ebb49edb2f273fd934ddc8f
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-08-04 14:46:30 +0600

    Edited

--- final.js
+++ final.js
@@ -1,13 +1,13 @@
 /** Problem -01 ( Divide the Asset ) */
 var area = 800;
-//write your code here
+//my code id written here
 var result = area/2;
 console.log(result);
 
 
 /** Problem -02 ( Cycle or Laptop ) */
 var money = 10000;
-//write your code here
+//my code id written here
 if (money>=25000) {
     console.log("Laptop");
 
@@ -22,7 +22,7 @@ if (money>=25000) {
 
 /** Problem -03 ( Medicine Planner ) */
 var lastDay = 11 ;
-//write your code here
+//my code id written here
 for (var i=1; i<=lastDay; i++) {
     if (i%3===0) {
         console.log(i+" - medicine");
@@ -36,7 +36,7 @@ for (var i=1; i<=lastDay; i++) {
 
 /** Problem 04 - (Delete / Store) */
 var fileName= "pdfData.jpg";
-//write your code here
+//my code id written here
 if (fileName.includes(".pdf") || fileName.includes(".docx") || fileName.includes("#")) {
     console.log("Store");
     
@@ -48,7 +48,7 @@ if (fileName.includes(".pdf") || fileName.includes(".docx") || fileName.includes
 
 /** Problem 05 - ( PH Email Generator )  */
 var student = { name: "jhankar", roll: 1014, department: "cse" };
-//write your code here
+//my code id written here
 var email = student.name + student.roll + "." + student.department + "@ph.ac.bd"
 console.log(email);
 
@@ -56,7 +56,7 @@ console.log(email);
 /** Problem 06 :  (Current Salary )  */
 var experience = 30;
 var startingSalary = 45000;
-//write your code here
+//my code id written here
 var currentSalary = startingSalary
 for (var i=1; i<=experience; i++) {
     currentSalary = currentSalary + (currentSalary*0.05)
