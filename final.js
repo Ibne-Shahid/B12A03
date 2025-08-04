5f39730fe8eade843202194a19f2a348bcfe4361
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-08-04 15:32:02 +0600

    Everything OK

--- final.js
+++ final.js
@@ -1,48 +1,51 @@
 /** Problem -01 ( Divide the Asset ) */
 var area = 800;
 //my code id written here
-var result = area/2;
+var result = area / 2;
 console.log(result);
 
 
 /** Problem -02 ( Cycle or Laptop ) */
 var money = 10000;
 //my code id written here
-if (money>=25000) {
+if (money >= 25000) {
     console.log("Laptop");
 
-} else if (money>=10000) {
+} else if (money >= 10000) {
     console.log("Cycle");
-    
+
 } else {
     console.log("Chocolate");
-    
+
 }
 
 
 /** Problem -03 ( Medicine Planner ) */
-var lastDay = 11 ;
+var lastDay = 11;
 //my code id written here
-for (var i=1; i<=lastDay; i++) {
-    if (i%3===0) {
-        console.log(i+" - medicine");
-        
-    } else {
-        console.log(i+" - rest");
-        
+if (lastDay >= 3 && lastDay <= 30) {
+    for (var i = 1; i <= lastDay; i++) {
+        if (i % 3 === 0) {
+            console.log(i + " - medicine");
+
+        } else {
+            console.log(i + " - rest");
+
+        }
     }
 }
 
 
+
 /** Problem 04 - (Delete / Store) */
-var fileName= "pdfData.jpg";
+var fileName = "#pdfData.jpg";
 //my code id written here
-if (fileName.includes(".pdf") || fileName.includes(".docx") || fileName.includes("#")) {
+if (fileName.includes(".pdf") || fileName.includes(".docx") || fileName[0] === "#") {
     console.log("Store");
-    
+
 } else {
     console.log("Delete");
-    
+
 }
 
 
@@ -57,8 +60,10 @@ console.log(email);
 var experience = 30;
 var startingSalary = 45000;
 //my code id written here
-var currentSalary = startingSalary
-for (var i=1; i<=experience; i++) {
-    currentSalary = currentSalary + (currentSalary*0.05)
+if (startingSalary > 0 && startingSalary <= 10000000 && experience > 0 && experience <= 50) {
+    var currentSalary = startingSalary
+    for (var i = 1; i <= experience; i++) {
+        currentSalary = currentSalary + (currentSalary * 5) / 100;
+    }
 }
 console.log(currentSalary.toFixed(2));
--- problem-3.js
+++ problem-3.js
@@ -1,12 +1,14 @@
 /** Problem -03 ( Medicine Planner ) */
-var lastDay = 11 ;
-//write your code here
-for (var i=1; i<=lastDay; i++) {
-    if (i%3===0) {
-        console.log(i+" - medicine");
-        
-    } else {
-        console.log(i+" - rest");
-        
+var lastDay = 11;
+//my code id written here
+if (lastDay >= 3 && lastDay <= 30) {
+    for (var i = 1; i <= lastDay; i++) {
+        if (i % 3 === 0) {
+            console.log(i + " - medicine");
+
+        } else {
+            console.log(i + " - rest");
+
+        }
     }
 }
